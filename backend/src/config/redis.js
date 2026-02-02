1d740bf05d4c8567687023069722a068acaaa030
Amey Ranaware <ranaware.amey@gmail.com>
2026-02-02 21:54:42 +0530

    chore: remove node_modules from git tracking

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-10088.c305.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 10088
+        host: 'redis-19446.crce206.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 19446
     }
 });
 
