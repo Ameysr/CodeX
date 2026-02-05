f472cd0850f633a0d0e29a7e42c55f6e079b0923
Amey Ranaware <ranaware.amey@gmail.com>
2026-02-05 22:33:42 +0530

    redis error fixed

--- backend/src/config/redis.js
+++ backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-19446.crce206.ap-south-1-1.ec2.cloud.redislabs.com',
-        port: 19446
+        host: 'redis-16012.crce276.ap-south-1-3.ec2.cloud.redislabs.com',
+        port: 16012
     }
 });
 
